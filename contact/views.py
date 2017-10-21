e8613d0f6130057bb5f0b4ce3d354bd3af9155e6
Mayur Gupta <mayurgupta0295@gmail.com>
2017-10-21 22:13:44 +0530

    ädded contact

---
+++ contact/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
+
+from .forms import contactForm
+
+# Create your views here.
+def contact(request):
+	title ='contact'
+	form = contactForm(request.POST or None)
+	confirm_message = None
+	#context = {'title': title, 'form': form, }
+	if form.is_valid():
+		name = form.cleaned_data['name']
+		comment = form.cleaned_data['comment']
+		subject = 'Message from Mysite.com'
+		message = '%s %s' %(comment, name)
+		emailFrom = form.cleaned_data['emails']
+		emailTo = [settings.EMAIL_HOST_USER]
+		send_mail(subject, message, emailFrom, emailTo, fail_silently=True )
+		title = "Thanks!"
+		confirm_message = "Thanks for message. we will get back right back to you."
+		form = None
+
+	context = {'title': title, 'form': form, 'confirm_message': confirm_message,}
+	template = "contact.html"
+	return render(request,template,context)
